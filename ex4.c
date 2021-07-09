662ec5d636751aaf7f2d47e7980c7c28acefaa57
hoshitatsu <87136211+hoshitatsu@users.noreply.github.com>
2021-07-09 12:13:20 +0900

    Greetings added

--- ex4.c
+++ ex4.c
@@ -4,21 +4,22 @@
 //ask name part
 int main(){
  srand(time(NULL));
-  int a, i,dice[2], total;
-  char str[10];
-printf("who are you?" );
-scanf("%s" ,str);
-printf("Hello, %s\n" ,str);
-//dice part
+ int a, i,dice[2], total;
+ char str[10];
  
-printf("please roll your dice...\n");
-for(a=0; a<2; a++){
+ printf("who are you?" );
+ scanf("%s" ,str);
+ printf("Hello, %s\n" ,str);
+ 
+ //dice part
+ printf("please roll your dice...\n");
+ for(a=0; a<2; a++){
   dice[a]=rand()%6+1;
-    printf("dice%d: %d\n",a+1,dice[a]);
-    total+=dice[a];
-}
+  printf("dice%d: %d\n",a+1,dice[a]);
+  total+=dice[a];
+ }
  printf("total: %d\n",total);
-if(total>7)printf("%s" won! ,str);
-  else printf("%s lose" ,name);
-  return 0;
+ if(total>7)printf("%s" won! ,str);
+ else printf("%s lose" ,name);
+ return 0;
 }
